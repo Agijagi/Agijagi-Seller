85551601763975d1ca57929a48407ed38a33f9f4
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-21 13:05:38 +0900

    Init: 프로젝트 초기 생성

---
+++ app/src/main/java/com/likelion/agijagiseller/global/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.likelion.agijagiseller.global
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.likelion.agijagiseller.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
