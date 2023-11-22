b82be12cf6975a3a27c260f1cceefa27286ef779
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-11-23 08:08:05 +0900

    [Feat] 네비게이션 연결 및 로그인 화면 구현 (#2)
    * Design: 로그인 화면 디자인 구현
    
    - 기존 LinearLayout에서 ConstraintLayout으로 변경
    
    * Feat: 메인 액티비티 네비게이션 연결

--- app/src/main/java/com/likelion/agijagiseller/global/MainActivity.kt
+++ app/src/main/java/com/likelion/agijagiseller/global/MainActivity.kt
@@ -2,11 +2,20 @@ package com.likelion.agijagiseller.global
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.likelion.agijagiseller.R
+import com.likelion.agijagiseller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
