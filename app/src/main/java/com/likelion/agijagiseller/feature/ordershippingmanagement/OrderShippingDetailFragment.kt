e11191c723a94524063953552a0d37aee9c7643c
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-12-05 11:02:37 +0900

    Design: OrderShippingDetailFragment 구현  #39 (#41)
    * Design: OrderShippingDetailFragment 구현  #39
    
    * Style: View Id 형식 수정  #39
    
    * Refactor: onDestroy() 수정  #39
    
    onDestroy() -> onDestroyView()

---
+++ app/src/main/java/com/likelion/agijagiseller/feature/ordershippingmanagement/OrderShippingDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.likelion.agijagiseller.feature.ordershippingmanagement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.likelion.agijagiseller.databinding.FragmentOrderShippingDetailBinding
+
+class OrderShippingDetailFragment : Fragment() {
+    private var _binding: FragmentOrderShippingDetailBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOrderShippingDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
