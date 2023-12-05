4fd6a33a0de74ef5af04cfc1851694958f37f68d
jklom0326 <jklom0326@gmail.com>
2023-12-05 10:36:33 +0900

    Refactor: onDestroy() 수정  #39
    onDestroy() -> onDestroyView()

--- app/src/main/java/com/likelion/agijagiseller/feature/ordershippingmanagement/OrderShippingDetailFragment.kt
+++ app/src/main/java/com/likelion/agijagiseller/feature/ordershippingmanagement/OrderShippingDetailFragment.kt
@@ -1,11 +1,10 @@
 package com.likelion.agijagiseller.feature.ordershippingmanagement
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.likelion.agijagiseller.R
+import androidx.fragment.app.Fragment
 import com.likelion.agijagiseller.databinding.FragmentOrderShippingDetailBinding
 
 class OrderShippingDetailFragment : Fragment() {
@@ -23,8 +22,8 @@ class OrderShippingDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
