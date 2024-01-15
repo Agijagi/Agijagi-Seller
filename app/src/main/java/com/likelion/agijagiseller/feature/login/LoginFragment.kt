8e7a2ce92b826aa7ac4df96a8b18c595c5930e73
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-16 04:28:11 +0900

    Feat: 카카오 소셜 회원가입 시 회원 정보 저장 로직 추가 #58

--- app/src/main/java/com/likelion/agijagiseller/feature/login/LoginFragment.kt
+++ app/src/main/java/com/likelion/agijagiseller/feature/login/LoginFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
@@ -24,7 +25,9 @@ class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    val db = Firebase.firestore
     private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -99,15 +102,14 @@ class LoginFragment : Fragment() {
                         UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
                     } else if (token != null) {
                         Log.d("카카오톡 로그인", "카카오톡으로 로그인 성공 ${token.accessToken}")
-                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
                         UserApiClient.instance.me { user, error ->
                             if (error != null) {
                                 Log.e("사용자 정보 요청 실패", "사용자 정보 요청 실패", error)
                             } else if (user != null) {
                                 val email = user.kakaoAccount?.email!!
                                 val password = user.id.toString()
-                                signUpKakaoAccount(email, password)
-                                Log.d("사용자 정보 요청 성공", user.kakaoAccount?.profile?.nickname.toString())
+                                val nickname = user.kakaoAccount?.profile?.nickname.toString()
+                                signUpKakaoAccount(email, password, nickname)
                             }
                         }
                     }
@@ -118,15 +120,37 @@ class LoginFragment : Fragment() {
         }
     }
 
-    private fun signUpKakaoAccount(email: String, password: String) {
+    private fun signUpKakaoAccount(
+        email: String,
+        password: String,
+        nickname: String,
+    ) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
+                    saveKakaoUserInfo(email, nickname)
                     Log.d("회원가입 성공", "카카오 회원가입 성공")
                 } else {
                     Log.w("회원가입 실패", "카카오 회원가입 실패", task.exception)
                 }
             }
+    }
+
+    private fun saveKakaoUserInfo(
+        email: String,
+        nickname: String,
+    ) {
+        val currentUser = auth.currentUser
+        currentUser?.let { user ->
+            val kakaoUser = hashMapOf(
+                "email" to email,
+                "loginType" to "KAKAO",
+                "name" to nickname
+            )
+            db.collection("seller").document(user.uid).set(kakaoUser).addOnSuccessListener {
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            }
+        }
     }
 
     override fun onDestroyView() {
