57e0a44021788f1ebc850d5f2f406650621ecb09
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-22 07:27:02 +0900

    Feat: GlobalApplication에 userDataStore 인스턴스 생성 #58

--- app/src/main/java/com/likelion/agijagiseller/global/GlobalApplication.kt
+++ app/src/main/java/com/likelion/agijagiseller/global/GlobalApplication.kt
@@ -1,9 +1,15 @@
 package com.likelion.agijagiseller.global
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.kakao.sdk.common.KakaoSdk
 import com.likelion.agijagiseller.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+
+val Context.userDataStore: DataStore<Preferences> by preferencesDataStore(name = "userPreferences")
 
 @HiltAndroidApp
 class GlobalApplication: Application() {
