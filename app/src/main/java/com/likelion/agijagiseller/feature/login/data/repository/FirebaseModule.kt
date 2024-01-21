991a0058347dfd1462be42664e3798aa339398c7
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-22 05:55:03 +0900

    Refactor: FirebaseModule 구현 #58

---
+++ app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/FirebaseModule.kt
@@ -0,0 +1,24 @@
+package com.likelion.agijagiseller.feature.login.data.repository
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFireStore(): FirebaseFirestore = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+}
