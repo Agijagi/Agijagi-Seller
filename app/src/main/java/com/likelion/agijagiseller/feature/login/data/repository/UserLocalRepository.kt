019268ec942bed2ac4687c23720a4e7796a8c452
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-22 09:29:13 +0900

    Feat: UserLocalRepository에 유저 정보 읽어오는 로직 구현 #58
    UserLocalRepository에 data store에 저장된 유저 정보 읽어오는 로직 구현

--- app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/UserLocalRepository.kt
+++ app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/UserLocalRepository.kt
@@ -1,5 +1,7 @@
 package com.likelion.agijagiseller.feature.login.data.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface UserLocalRepository {
     suspend fun updateUid(uid: String)
 
@@ -8,4 +10,12 @@ interface UserLocalRepository {
     suspend fun updateLoginType(loginType: String)
 
     suspend fun updateName(name: String)
+
+    val uid: Flow<String?>
+
+    val email: Flow<String?>
+
+    val loginType: Flow<String?>
+
+    val name: Flow<String?>
 }
--- app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/UserLocalRepositoryImpl.kt
+++ app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/UserLocalRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.likelion.agijagiseller.feature.login.data.repository
 
 import com.likelion.agijagiseller.feature.login.data.local.UserLocalDataSource
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserLocalRepositoryImpl @Inject constructor(
@@ -13,4 +14,16 @@ class UserLocalRepositoryImpl @Inject constructor(
     override suspend fun updateLoginType(loginType: String) = userLocalDataSource.updateLoginType(loginType)
 
     override suspend fun updateName(name: String) = userLocalDataSource.updateName(name)
+
+    override val uid: Flow<String?>
+        get() = userLocalDataSource.uid
+
+    override val email: Flow<String?>
+        get() = userLocalDataSource.email
+
+    override val loginType: Flow<String?>
+        get() = userLocalDataSource.loginType
+
+    override val name: Flow<String?>
+        get() = userLocalDataSource.name
 }
