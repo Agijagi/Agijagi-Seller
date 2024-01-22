1c69f2b27f897375d15e3f7c6b0bde0226d17a8f
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-22 10:50:30 +0900

    Remove: FirebaseModule 삭제 #58
    FirebaseModule 내 메서드 UserModule로 이동 후 FirebaseModule 삭제

--- app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/UserModule.kt
+++ app/src/main/java/com/likelion/agijagiseller/feature/login/data/repository/UserModule.kt
@@ -1,5 +1,10 @@
 package com.likelion.agijagiseller.feature.login.data.repository
 
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -9,6 +14,14 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object UserModule {
+    @Provides
+    @Singleton
+    fun provideFireStore(): FirebaseFirestore = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+
     @Provides
     @Singleton
     fun provideUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository {
