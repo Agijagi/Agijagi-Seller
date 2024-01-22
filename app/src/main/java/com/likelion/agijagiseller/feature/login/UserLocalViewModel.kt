a8347618fee6a244a5a80b747e1c137e8961a08d
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-22 09:55:26 +0900

    Feat: UserLocalViewModel에 유저 정보 읽어오는 로직 구현 #58
    UserLocalViewMode에 data store에 저장된 유저 정보 읽어오는 로직 구현

--- app/src/main/java/com/likelion/agijagiseller/feature/login/UserLocalViewModel.kt
+++ app/src/main/java/com/likelion/agijagiseller/feature/login/UserLocalViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.likelion.agijagiseller.feature.login.data.repository.UserLocalRepositoryImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,6 +13,18 @@ import javax.inject.Inject
 class UserLocalViewModel @Inject constructor(
     private val userLocalRepositoryImpl: UserLocalRepositoryImpl,
 ) : ViewModel() {
+    private var _uid = MutableLiveData<String>()
+    val uid get() = _uid
+
+    private var _email = MutableLiveData<String>()
+    val email get() = _email
+
+    private var _loginType = MutableLiveData<String>()
+    val loginType get() = _loginType
+
+    private var _name = MutableLiveData<String>()
+    val name get() = _name
+
     fun updateUid(uid: String) =
         viewModelScope.launch {
             userLocalRepositoryImpl.updateUid(uid)
@@ -31,4 +44,36 @@ class UserLocalViewModel @Inject constructor(
         viewModelScope.launch {
             userLocalRepositoryImpl.updateName(name)
         }
+
+    fun fetchUid() {
+        viewModelScope.launch {
+            userLocalRepositoryImpl.uid.collect { uid ->
+                _uid.value = uid
+            }
+        }
+    }
+
+    fun fetchEmail() {
+        viewModelScope.launch {
+            userLocalRepositoryImpl.email.collect { email ->
+                _email.value = email
+            }
+        }
+    }
+
+    fun fetchLoginType() {
+        viewModelScope.launch {
+            userLocalRepositoryImpl.loginType.collect { loginType ->
+                _loginType.value = loginType
+            }
+        }
+    }
+
+    fun fetchName() {
+        viewModelScope.launch {
+            userLocalRepositoryImpl.name.collect { name ->
+                _name.value = name
+            }
+        }
+    }
 }
